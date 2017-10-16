e7b988d40e5c93d661f4995dca8f7e019eb47eab
alexsan134 <as62971@gmail.com>
2017-10-16 00:38:58 +0000

    Update sketch_oct15a.ino

--- sketch_oct15a.ino
+++ sketch_oct15a.ino
@@ -7,7 +7,7 @@ LiquidCrystal lcd(1, 2, 4, 5, 6, 7);
 const int btn = 8;
 const int rst = 9;
 const int bs = 3;
-const int nModes= 3;
+const int nModes= 3; //Numero de Modos
 int h = 0;
 int m = 0;
 volatile int s = 0;
@@ -47,7 +47,7 @@ void updateClock(){
     }
 }
 
-void playSound(){
+void playSound(){//reproducir sonido en buzzer(bs) pin#3
     tone(bs, 900, 300);
 }
 
@@ -55,12 +55,12 @@ void change(){
     mode++;
     mode = mode % nModes;
     update=true;
-    switch(mode){
-        case 0: modeText= "p";
+    switch(mode){ //Mostrar texto opcional sobre el modo
+        case 0: modeText= "p"; //p Pausa
         break;
-        case 1: modeText = "r";
+        case 1: modeText = "r";//r Reset
         break;
-        case 2: modeText = "N";
+        case 2: modeText = "N";//N None
         break;
     }
     lcd.clear();
@@ -69,7 +69,7 @@ void change(){
 void setMode(){
     lcd.clear();
     switch(mode){
-    case 0 :
+    case 0 ://Si modo == 0 pausa
         pause = !pause;
         if(pause==true){
             lcd.print("    Pausado");
@@ -78,16 +78,16 @@ void setMode(){
             sendPrint();
         }
     break;
-    case 1:
+    case 1://Si modo == 1 reset
         s=0;m=0;h=0;
         sendPrint();
     break;
-    case 2: break;
+    case 2: break;// Si modo == 2 do something
      }
     
 }
 
-void sendPrint(){
+void sendPrint(){ //Imprimir variables en lcd (Cambiar funcion para imprimir en otro lado o solo guardar los datos
     lcd.setCursor(4,0);
       sprintf(clockText, "%02d:%02d:%02d", h,m,s);
         lcd.print(clockText);
@@ -96,13 +96,13 @@ void sendPrint(){
 }
 
 void loop(){
-    if(digitalRead(btn)==HIGH){
+    if(digitalRead(btn)==HIGH){//Boton para cambiar de modos
         delay(20);
         digitalWrite(btn, HIGH);
        change();
     while(digitalRead(btn)==HIGH);
     }
-    if(digitalRead(rst)==HIGH){
+    if(digitalRead(rst)==HIGH){//Boton para seleccionar modo
         delay(20);
         digitalWrite(rst, HIGH);
         setMode();
